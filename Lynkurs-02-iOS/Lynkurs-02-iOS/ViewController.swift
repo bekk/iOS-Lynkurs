12d0cce0dd7ac7ba08008746817236fb706ce11a
Oscar Eckbo <oscareckbo@mac-oscareckbo.local>
2017-02-15 13:04:19 +0100

    Legger til funksjonalitet for å endre label vha. input

--- Lynkurs-02-iOS/Lynkurs-02-iOS/ViewController.swift
+++ Lynkurs-02-iOS/Lynkurs-02-iOS/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var input: UITextField!
+
+    @IBAction func button(_ sender: Any) {
+        if ((input.text) != nil) {
+            oppdaterLabel(inputText: input.text!)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func oppdaterLabel(inputText: String) {
+        label.text = inputText;
+    }
 }
-
