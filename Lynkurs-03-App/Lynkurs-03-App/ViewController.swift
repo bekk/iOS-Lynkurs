66c9a2fd937542379f0e396477281d0f2c7de3f5
Fredrik Lillejordet <fretholil@gmail.com>
2017-02-15 12:07:46 +0100

    startet på appen

---
+++ Lynkurs-03-App/Lynkurs-03-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lynkurs-03-App
+//
+//  Created by Fredrik Lillejordet on 15.02.2017.
+//  Copyright © 2017 Bekk Consulting AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
