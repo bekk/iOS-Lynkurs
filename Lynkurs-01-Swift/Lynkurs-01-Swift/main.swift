51d7efe910e2c758ecfb18af397f18e30e9cf938
Fredrik Lillejordet <fretholil@gmail.com>
2017-02-15 12:28:09 +0100

    Lagt til Kardashians

---
+++ Lynkurs-01-Swift/Lynkurs-01-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lynkurs-01-Swift
+//
+//  Created by Fredrik Lillejordet on 15.02.2017.
+//  Copyright © 2017 Bekk Consulting AS. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
