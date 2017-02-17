6c858d50174f95143f3ead65f1952da46614c74e
Fredrik Lillejordet <fretholil@gmail.com>
2017-02-17 14:56:39 +0100

    konverterings-view ferdig

--- Lynkurs-03-App/Lynkurs-03-App/ConverterViewController.swift
+++ Lynkurs-03-App/Lynkurs-03-App/ConverterViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ConverterViewController: UIViewController {
     
+    @IBOutlet weak var fahrenheit: UITextField!
+    @IBOutlet weak var celsius: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Converter"
+
+        fahrenheit.addTarget(self, action: #selector(textFieldValueChanged(_:)), for: .editingChanged)
+        
+        celsius.addTarget(self, action: #selector(textFieldValueChanged(_:)), for: .editingChanged)
     }
     
+    func textFieldValueChanged(_ textField : UITextField) {
+        
+        if textField == fahrenheit {
+            if let value : Double = Double(fahrenheit.text!){
+                let converted = round((value - 32) / 1.8)
+                celsius.text = "\(converted)"
+            }
+        }else{
+            if let value : Double = Double(celsius.text!){
+                let converted = round(value * 1.8 + 32)
+                fahrenheit.text = "\(converted)"
+            }
+        }
+    }
 }
