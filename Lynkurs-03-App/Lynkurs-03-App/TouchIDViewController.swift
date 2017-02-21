62426c780464562adfc5a98a482d1def56b001d3
Fredrik Lillejordet <fretholil@gmail.com>
2017-02-21 17:47:41 +0100

    refaktorert formatering

--- Lynkurs-03-App/Lynkurs-03-App/TouchIDViewController.swift
+++ Lynkurs-03-App/Lynkurs-03-App/TouchIDViewController.swift
@@ -14,6 +14,7 @@ import LocalAuthentication
 class TouchIDViewController : UIViewController {
     
     @IBOutlet weak var message: UILabel!
+    
     var context = LAContext()
     
     override func viewDidLoad() {
@@ -26,7 +27,7 @@ class TouchIDViewController : UIViewController {
         
         var error: NSError?
         guard context.canEvaluatePolicy(policy, error: &error) else {
-            print("Error: \(error)")
+            print("Error: canEvaluatePolicy \(error)")
             return
         }
         
