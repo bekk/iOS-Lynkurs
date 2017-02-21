03944aef4f1520f652613ccbe6e463ddc9ab4e15
Fredrik Lillejordet <fretholil@gmail.com>
2017-02-21 08:09:46 +0100

    fjernet 01-løsning

--- Lynkurs-01-Swift/Lynkurs-01-Swift/Person.swift
+++ Lynkurs-01-Swift/Lynkurs-01-Swift/Person.swift
@@ -9,46 +9,35 @@
 import Foundation
 
 class Person {
-
+    
     let firstName, lastName, gender: String
     var siblings: [Person] = []
     var age: Int
-
+    
     init(firstName: String, lastName: String, gender: String, age: Int) {
-        self.firstName = firstName
-        self.lastName = lastName
-        self.gender = gender
-        self.age = age
-    }
 
+    }
+    
     func fullName() -> String {
-        return "\(firstName) \(lastName)"
+        return ""
     }
-
+    
     func canBuyBeerIn(country: String) -> Bool {
-        switch country {
-        case "USA":
-            return age >= 21
-        case "Norway":
-            return age >= 18
-        default:
-            return false
-        }
+        return true
     }
-
+    
     func addSibling(sibling: Person) {
-        siblings.append(sibling)
     }
-
+    
     func getNumberOfSiblings() -> Int {
-        return siblings.count
+        return 0
     }
-
+    
     func getFirstNameOfSiblings() -> [String] {
-        return siblings.map {$0.firstName}
+        return []
     }
-
+    
     func getFirstNameOfSisters() -> [String] {
-        return siblings.filter {$0.gender == "Female"}.map {$0.firstName}
+        return []
     }
 }
