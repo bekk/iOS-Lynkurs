c833f5066dd19e85949daeca2fc2909650e0a5a0
Fredrik Lillejordet <fretholil@gmail.com>
2017-02-23 10:33:05 +0100

    lagt til oppdatert løsning

--- Lynkurs-01-Swift/Lynkurs-01-Swift/Person.swift
+++ Lynkurs-01-Swift/Lynkurs-01-Swift/Person.swift
@@ -5,7 +5,6 @@
 //  Created by Fredrik Lillejordet on 15.01.2017.
 //  Copyright © 2017 Bekk Consulting AS. All rights reserved.
 //
-
 import Foundation
 
 class Person {
@@ -15,32 +14,40 @@ class Person {
     var age: Int
 
     init(firstName: String, lastName: String, gender: String, age: Int) {
-        self.firstName = ""
-        self.lastName = ""
-        self.gender = ""
-        self.age = 0
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
+        self.age = age
     }
 
     func fullName() -> String {
-        return ""
+        return "\(firstName) \(lastName)"
     }
 
     func canBuyBeerIn(country: String) -> Bool {
-        return true
+        switch country {
+        case "USA":
+            return age >= 21
+        case "Norway":
+            return age >= 18
+        default:
+            return false
+        }
     }
 
     func addSibling(sibling: Person) {
+        siblings.append(sibling)
     }
 
     func getNumberOfSiblings() -> Int {
-        return 0
+        return siblings.count
     }
 
     func getFirstNameOfSiblings() -> [String] {
-        return []
+        return siblings.map {$0.firstName}
     }
 
     func getFirstNameOfSisters() -> [String] {
-        return []
+        return siblings.filter {$0.gender == "Female"}.map {$0.firstName}
     }
 }
